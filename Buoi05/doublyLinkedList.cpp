7a61e87927f1c839b25bb8183e3ca581c676aab5
Truong Tri Hung <25ad11008@student.hcmute.edu.vn>
2026-09-09 16:03:13 +0700

    Change file's names

--- Buoi05/doublyLinkedList.cpp
+++ Buoi05/doublyLinkedList.cpp
@@ -6,6 +6,33 @@ struct Node {
     struct Node *next, *prev;
 };
 
+void addFirst(Node *&head, int x);
+
+void addLast(Node *&head, int x);
+
+void printList(Node *n);
+
+void printListLF(Node *n);
+
+int main() {
+    Node *head1 = NULL, *head2 = NULL, *head3 = NULL;
+    for (int x = 1; x < 21; x = x + 2) {
+        addLast(head1, x);
+    }
+    for (int x = 2; x < 21; x = x + 2) {
+        addLast(head2, x);
+    }
+    cout << "\n\n====LIST 1: Start to Last====\n";
+    printList(head1);
+    cout << "\n\n====LIST 1: Last to Start====\n";
+    printListLF(head1);
+    cout << "\n\n====LIST 2: Start to Last====\n";
+    printList(head2);
+    cout << "\n\n====LIST 2: Last to Start====\n";
+    printListLF(head2);
+    return 0;
+}
+
 void addFirst(Node *&head, int x) {
     Node *p = new Node();
     p->data = x;
@@ -44,19 +71,4 @@ void printListLF(Node *n) {
         cout << n->data << "<-";
         n = n->prev;
     }
-}
-
-int main() {
-    Node *head1 = NULL, *head2 = NULL, *head3 = NULL;
-    for (int x = 1; x < 21; x = x + 2) {
-        addLast(head1, x);
-    }
-    for (int x = 2; x < 21; x = x + 2) {
-        addLast(head2, x);
-    }
-    cout << "\n\n====LIST 1: Start to Last====\n"; printList(head1);
-    cout << "\n\n====LIST 1: Last to Start====\n"; printListLF(head1);
-    cout << "\n\n====LIST 2: Start to Last====\n"; printList(head2);
-    cout << "\n\n====LIST 2: Last to Start====\n"; printListLF(head2);
-    return 0;
 }
