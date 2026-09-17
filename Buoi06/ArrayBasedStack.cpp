a5de680855a40e1b34d6b2dd2454d5c191b37c06
Truong Tri Hung <25ad11008@student.hcmute.edu.vn>
2026-09-17 22:31:35 +0700

    idk

--- Buoi06/ArrayBasedStack.cpp
+++ Buoi06/ArrayBasedStack.cpp
@@ -1,68 +1,74 @@
 #include<iostream>
 using namespace std;
+
 #define MAX 100
+
 class Stack {
-    int top=-1;
+    int top = -1;
     int data[MAX];
+
 public:
     Stack() { top = -1; }
     ~Stack() { top = -1; }
+
     bool push(int x);
+
     int pop();
+
     int getTop();
-    bool isEmpty()
-    {return (top < 0);};
-    bool isFull()
-    {return (top >= MAX-1);
+
+    bool isEmpty() { return (top < 0); };
+
+    bool isFull() {
+        return (top >= MAX - 1);
     };
 };
-bool Stack::push(int x)
-{
-    if (top >= (MAX - 1)){
+
+bool Stack::push(int x) {
+    if (top >= (MAX - 1)) {
         cout << "Overflow";
         return false;
-    }
-    else {
+    } else {
         data[++top] = x;
         return true;
     }
 }
-int Stack::pop()
-{
+
+int Stack::pop() {
     if (top < 0) {
-        cout <<"Underflow"; return 0;
-    }
-    else {
-        return data[top--]; ;
+        cout << "Underflow";
+        return 0;
+    } else {
+        return data[top--];;
     }
 }
-int Stack::getTop()
-{
+
+int Stack::getTop() {
     if (top < 0) {
-        cout << "is Empty"; return 0;
-    }
-    else {
+        cout << "is Empty";
+        return 0;
+    } else {
         return data[top];
     }
 }
-int main()
-{
+
+int main() {
     Stack s;
-    cout <<"\n ===Before push elements====";
-    cout <<"\n Check empty: "<<s.isEmpty();
-    cout <<"\n Check full: "<<s.isFull()<<endl;
-    for (int i=1;i<=100;i++){
+    cout << "\n ===Before push elements====";
+    cout << "\n Check empty: " << s.isEmpty();
+    cout << "\n Check full: " << s.isFull() << endl;
+    for (int i = 1; i <= 100; i++) {
         s.push(i);
     }
-    cout <<"\n ===After push elements====";
-    cout <<"\n Check empty: "<<s.isEmpty();
-    cout <<"\n Check full: "<<s.isFull();
-    cout <<"\n === Pop all elements===="<<endl;
-    for (int i=1;i<=100;i++){
-        cout << s.pop() <<"->";
+    cout << "\n ===After push elements====";
+    cout << "\n Check empty: " << s.isEmpty();
+    cout << "\n Check full: " << s.isFull();
+    cout << "\n === Pop all elements====" << endl;
+    for (int i = 1; i <= 100; i++) {
+        cout << s.pop() << "->";
     }
-    cout <<"\n ===After pop all elements====";
-    cout <<"\n Check empty: "<<s.isEmpty();
-    cout <<"\n Check full: "<<s.isFull()<<endl;
+    cout << "\n ===After pop all elements====";
+    cout << "\n Check empty: " << s.isEmpty();
+    cout << "\n Check full: " << s.isFull() << endl;
     return 0;
 }
--- Buoi06/PointerBasedStack.cpp
+++ Buoi06/PointerBasedStack.cpp
@@ -1,23 +1,29 @@
 #include<iostream>
 #define INF 9999999;
 using namespace std;
+
 struct Node {
     int data;
-    struct Node* link;
+    struct Node *link;
 };
+
 class Stack {
 public:
-    struct Node* top;
+    struct Node *top;
     Stack() { top = NULL; }
     ~Stack() { top = NULL; }
+
     bool push(int x);
+
     int pop();
+
     int getTop();
-    bool isEmpty() {return (top==NULL);};
+
+    bool isEmpty() { return (top == NULL); };
 };
-bool Stack::push(int data)
-{
-    struct Node* temp;
+
+bool Stack::push(int data) {
+    struct Node *temp;
     temp = new Node();
     if (!temp) {
         cout << "\nHeap Overflow";
@@ -28,39 +34,38 @@ bool Stack::push(int data)
     top = temp;
     return true;
 }
-int Stack::pop()
-{
-    struct Node* temp;
+
+int Stack::pop() {
+    struct Node *temp;
     if (top == NULL) {
-        cout << "\nStack Underflow"<< endl;
+        cout << "\nStack Underflow" << endl;
         return INF;
-    }
-    else {
+    } else {
         temp = top;
         top = top->link;
         temp->link = NULL;
-        int data=temp->data;
+        int data = temp->data;
         free(temp);
         return data;
     }
 }
-int main()
-{
+
+int main() {
     Stack s;
-    cout <<"\n ===Before push elements====";
-    cout <<"\n Check empty: "<<s.isEmpty();
-    for (int i=1;i<=100;i++){
+    cout << "\n ===Before push elements====";
+    cout << "\n Check empty: " << s.isEmpty();
+    for (int i = 1; i <= 100; i++) {
         s.push(i);
     }
-    cout <<"\n ===After push elements====";
-    cout <<"\n Check empty: "<<s.isEmpty();
+    cout << "\n ===After push elements====";
+    cout << "\n Check empty: " << s.isEmpty();
     cout << "\n Top element is " << s.getTop();
-    cout <<"\n === Pop all elements===="<<endl;
-    for (int i=1;i<=100;i++){
-        cout << s.pop() <<"->";
+    cout << "\n === Pop all elements====" << endl;
+    for (int i = 1; i <= 100; i++) {
+        cout << s.pop() << "->";
     }
     cout << "\n Top element is " << s.getTop();
-    cout <<"\n ===After pop all elements====";
-    cout <<"\n Check empty: "<<s.isEmpty();
+    cout << "\n ===After pop all elements====";
+    cout << "\n Check empty: " << s.isEmpty();
     return 0;
 }
