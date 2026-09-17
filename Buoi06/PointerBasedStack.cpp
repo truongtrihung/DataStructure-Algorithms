c3dec0604bf0494cc539a6a0d1a8cef3adbe231f
Truong Tri Hung <25ad11008@student.hcmute.edu.vn>
2026-09-17 22:25:12 +0700

    Updating pointer-stack

--- Buoi06/PointerBasedStack.cpp
+++ Buoi06/PointerBasedStack.cpp
@@ -1,16 +1,66 @@
-#include <iostream>
-
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+#include<iostream>
+#define INF 9999999;
+using namespace std;
+struct Node {
+    int data;
+    struct Node* link;
+};
+class Stack {
+public:
+    struct Node* top;
+    Stack() { top = NULL; }
+    ~Stack() { top = NULL; }
+    bool push(int x);
+    int pop();
+    int getTop();
+    bool isEmpty() {return (top==NULL);};
+};
+bool Stack::push(int data)
+{
+    struct Node* temp;
+    temp = new Node();
+    if (!temp) {
+        cout << "\nHeap Overflow";
+        return false;
     }
-
+    temp->data = data;
+    temp->link = top;
+    top = temp;
+    return true;
+}
+int Stack::pop()
+{
+    struct Node* temp;
+    if (top == NULL) {
+        cout << "\nStack Underflow"<< endl;
+        return INF;
+    }
+    else {
+        temp = top;
+        top = top->link;
+        temp->link = NULL;
+        int data=temp->data;
+        free(temp);
+        return data;
+    }
+}
+int main()
+{
+    Stack s;
+    cout <<"\n ===Before push elements====";
+    cout <<"\n Check empty: "<<s.isEmpty();
+    for (int i=1;i<=100;i++){
+        s.push(i);
+    }
+    cout <<"\n ===After push elements====";
+    cout <<"\n Check empty: "<<s.isEmpty();
+    cout << "\n Top element is " << s.getTop();
+    cout <<"\n === Pop all elements===="<<endl;
+    for (int i=1;i<=100;i++){
+        cout << s.pop() <<"->";
+    }
+    cout << "\n Top element is " << s.getTop();
+    cout <<"\n ===After pop all elements====";
+    cout <<"\n Check empty: "<<s.isEmpty();
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
